0810c250659d31771cd8199b427e40229e00e841
pengode-handal <saifpatih@gmail.com>
2023-08-26 03:42:04 +0700

    Merapikan file cuy

---
+++ git.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+x = open("environments.json")
+data = json.load(x)
+
+usrname = data['username']
+email = data["email"]
+
+os.system(f"git config --global user.name \"{usrname}\"")
+os.system(f"git config --global user.email \"{email}\"")
+os.system("git init")
+os.system("git add .")
+os.system("git remote add origin https://github.com/pengode-handal/P5-3B")
+pesan = input("Masukin pesan: ")
+os.system(f"git commit -m \"{pesan}\"")
+os.system(f"git branch -M main")
+os.system(f"git push -f -u origin main")
