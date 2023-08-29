f8d6dc1897b452968360d4f529c9bb5befa2ec33
radhhh <123277320+radhhh@users.noreply.github.com>
2023-08-29 17:24:36 +0700

    Merging sub-pages

---
+++ pages/sp/git.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+x = open("environments.json")
+data = json.load(x)
+
+usrname = data['username']
+email = data["email"]
+branch = data['branch']
+
+os.system(f"git config --global user.name \"{usrname}\"")
+os.system(f"git config --global user.email \"{email}\"")
+os.system("git init")
+os.system("git add .")
+os.system("git remote add origin https://github.com/pengode-handal/P5-3B")
+pesan = input("Masukin pesan: ")
+os.system(f"git commit -m \"{pesan}\"")
+os.system(f"git branch -M {branch}")
+os.system(f"git push -f -u origin {branch}")
